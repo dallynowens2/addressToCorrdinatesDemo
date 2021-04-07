a6c801dbc83676d7de33a669839f343b0254dcd0
dallynowens2 <dallynowens2@gmail.com>
2021-04-07 12:40:35 -0600

    Add project files.

---
+++ addressToCorrdinates/addressToCorrdinates/MainPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace addressToCorrdinates
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var result = await Geocoding.GetLocationsAsync(entry.Text);
+
+                if(result.Any())
+                    resultLocation.Text = $"lat: {result.FirstOrDefault()?.Latitude}, long:{result.FirstOrDefault()?.Longitude}";
+
+            }
+            catch(FeatureNotSupportedException notSupporedex)
+            {
+
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+
+        async void Reverse_Button_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double lat;
+                double lng;
+
+                lat = Convert.ToDouble(entry.Text.Split(',')[0]);
+                lng = Convert.ToDouble(entry.Text.Split(',')[1]);
+
+                var result = await Geocoding.GetPlacemarksAsync(lat, lng);
+
+                if (result.Any())
+                    resultLocation.Text = result.FirstOrDefault()?.FeatureName;
+            }
+            catch (FeatureNotSupportedException notSupporedex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+        }
+    }
+}
